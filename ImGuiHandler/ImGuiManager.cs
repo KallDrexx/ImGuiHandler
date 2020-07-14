8f57d46ad4f0a4f19abb8cb70b4df8e037866a5a
KallDrexx <me@mshapiro.net>
2020-07-13 20:18:29 -0400

    Queue up add/remove requests
    Sometimes `AddElement` and `RemoveElement` were being triggered via events, and that was causing an exception due to the element collection being modified during iteration.
    
    This fixes that by queuing all add/remove requests and performing them in a batch prior to iteration.

--- ImGuiHandler/ImGuiManager.cs
+++ ImGuiHandler/ImGuiManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ImGuiNET;
 
 namespace ImGuiHandler
@@ -7,7 +8,9 @@ namespace ImGuiHandler
     public class ImGuiManager
     {
         private readonly ImGuiRenderer _renderer;
-        private readonly List<ImGuiElement> _elements = new List<ImGuiElement>();
+        private readonly HashSet<ImGuiElement> _elements = new HashSet<ImGuiElement>();
+        private readonly Queue<ImGuiElement> _elementsToAdd = new Queue<ImGuiElement>();
+        private readonly Queue<ImGuiElement> _elementsToRemove = new Queue<ImGuiElement>();
         
         /// <summary>
         /// If true, ImGui elements are currently accepting mouse input.  This can be used to detect if the
@@ -37,7 +40,7 @@ namespace ImGuiHandler
         /// <param name="element"></param>
         public void AddElement(ImGuiElement element)
         {
-            _elements.Add(element);
+            _elementsToAdd.Enqueue(element);
         }
 
         /// <summary>
@@ -45,7 +48,7 @@ namespace ImGuiHandler
         /// </summary>
         public void RemoveElement(ImGuiElement element)
         {
-            _elements.Remove(element);
+            _elementsToRemove.Enqueue(element);
         }
 
         /// <summary>
@@ -53,6 +56,21 @@ namespace ImGuiHandler
         /// </summary>
         public void RenderElements(TimeSpan timeSinceLastFrame)
         {
+            // Add and remove all elements waiting to be worked on.  We can't do this when `AddElement` or
+            // `RemoveElement` is called as events might trigger them, and that will cause the collection to be 
+            // modified during iteration
+            while (_elementsToAdd.Any())
+            {
+                var element = _elementsToAdd.Dequeue();
+                _elements.Add(element);
+            }
+
+            while (_elementsToRemove.Any())
+            {
+                var element = _elementsToRemove.Dequeue();
+                _elements.Remove(element);
+            }
+            
             _renderer.BeforeLayout(timeSinceLastFrame);
             foreach (var element in _elements)
             {
